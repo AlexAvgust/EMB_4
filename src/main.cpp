767cf3704cecb1afe5e52acdb6ea2f54f08c9c6f
Oleksii <oleksii@customer.wrswpol1.isp.starlink.com>
2026-02-20 20:56:51 +0200

    changed gpio names

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+// PINS
+#define LED_RED 15
+#define LED_GREEN 16
+#define LED_BLUE 17
 
-void led_init(){
-  pinMode(15, OUTPUT);
-  pinMode(16, OUTPUT);
-  pinMode(17, OUTPUT);
-  for (u_int8_t i = 15; i <= 17; i++)
-  {
-    digitalWrite(i, LOW);
-  }
+void led_init()
+{
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_BLUE, LOW);
 }
 
 void led_blink(u_int8_t ledPin, u_int16_t delayTime)
@@ -21,13 +26,11 @@ void led_blink(u_int8_t ledPin, u_int16_t delayTime)
 void setup()
 {
   led_init();
-
 }
 
 void loop()
 {
-  led_blink(15, 600);
-  led_blink(16, 600);
-  led_blink(17, 600);
-  
+  led_blink(LED_RED, 600);
+  led_blink(LED_GREEN, 600);
+  led_blink(LED_BLUE, 600);
 }
